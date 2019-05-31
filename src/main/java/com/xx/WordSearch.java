17478227d31367d723e9a2c0150ac4dd927501b9
xxBeam <370967523@qq.com>
2019-05-31 14:47:05 +0800

    79. 单词搜索

---
+++ src/main/java/com/xx/WordSearch.java
@@ -0,0 +1,46 @@
+package com.xx;
+
+/**
+ * 79. 单词搜索
+ */
+public class WordSearch {
+
+    public boolean exist(char[][] board, String word) {
+        if(word.isEmpty()){
+            return false;
+        }
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if(checkWord(word,board,i,j,0)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean checkWord(String word, char[][] board, int i, int j, int x){
+        if(x==word.length()){
+            return true;
+        }
+        if((i<0||i>=board.length||j<0||j>=board[0].length||board[i][j]!=word.charAt(x))){
+            return false;
+        }else{
+            board[i][j] += 256;
+            if(checkWord(word,board,i-1,j,x+1)||checkWord(word,board,i+1,j,x+1)||checkWord(word,board,i,j-1,x+1)||checkWord(word,board,i,j+1,x+1)){
+                return true;
+            }
+            board[i][j] -= 256;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        char board[][] = {{'A','B','C','E'},{'S','F','E','S'},{'A','D','E','E'}};
+        System.out.println(new WordSearch().exist(board,"ABCESEEEFS"));
+
+
+        char a = 'B';
+        System.out.println(a+=256);
+    }
+}
